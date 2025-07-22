d6800e06251049673dc7c9f49fe20b1ab4372d97
Jinwoo Kim <jinwookim@kw.ac.kr>
2025-07-22 21:46:28 +0900

    vast.ai setup script

---
+++ setup.sh
@@ -0,0 +1,11 @@
+git clone https://github.com/PKU-YuanGroup/Video-LLaVA
+cd Video-LLaVA
+conda create -n videollava python=3.10 -y
+conda activate videollava
+pip install --upgrade pip  # enable PEP 660 support
+pip install -e .
+pip install -e ".[train]"
+pip install flash-attn --no-build-isolation
+pip install decord opencv-python git+https://github.com/facebookresearch/pytorchvideo.git@28fe037d212663c6a24f373b94cc5d478c8c1a1d
+pip install gdown
+gdown 1bRJuqu-rvlwmEPgafoENHVxG_Spj9QK7
