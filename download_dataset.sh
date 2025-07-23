dc537fbb6be04d3f6865f5a7c296d59601420427
Jinwoo Kim <jinwookim@kw.ac.kr>
2025-07-23 14:15:31 +0900

    test script

---
+++ download_dataset.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+kaggle datasets download -d yassershrief/hockey-fight-vidoes
+kaggle datasets download -d mohamedmustafa/real-life-violence-situations-dataset
+
+unzip hockey-fight-vidoes.zip
+unzip real-life-violence-situations-dataset.zip
+
+
+
